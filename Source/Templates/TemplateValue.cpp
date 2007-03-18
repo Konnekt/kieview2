7ed13afe0632e0ce23c9a33ffe687929f743d1b8
Ursus <ursus6@wp.pl>
2007-03-18 16:00:55 +0000

    *obsluga kilku rodzajow wartosci dla zmiennych w szablonach
    git-svn-id: http://www.konnekt.info/svn/kieview2/trunk@319 19bc53de-b422-0410-9685-d915b0f5fb0a

--- Source/Templates/TemplateValue.cpp
+++ Source/Templates/TemplateValue.cpp
@@ -1,1 +1,260 @@
 #include "stdafx.h"
+#include "TemplateValue.h"
+
+TemplateVar::TemplateVar(const string& name, bool isFunction) {
+  _name = name;
+  _isFunction = isFunction;
+}
+
+TemplateVar::TemplateVar(const TemplateVar& copy) {
+  this->copy((TemplateVar&) copy);
+}
+
+const TemplateVar& TemplateVar::operator = (const TemplateVar& copy) {
+  if (this == &copy) return *this;
+  this->copy((TemplateVar&) copy);
+  return *this;
+}
+
+TemplateVar& TemplateVar::operator = (TemplateVar& copy) {
+  if (this == &copy) return *this;
+  this->copy(copy);
+  return *this;
+}
+
+TemplateValue TemplateVar::get() {
+  return TemplateValue();
+}
+
+TemplateVar::~TemplateVar() {
+  for (tParams::iterator it = _params.begin(); it != _params.end(); it++) {
+    delete (*it);
+  }
+}
+void TemplateVar::copy(TemplateVar& value) {
+  _isFunction = value.isFunction();
+  _name = value._name;
+  for (tParams::iterator it = value._params.begin(); it != value._params.end(); it++) {
+//    _params.push_back(new TemplateParam((*it));
+  }
+}
+
+bool TemplateVar::isFunction() {
+  return _isFunction;
+}
+
+
+TemplateValue::TemplateValue(const TemplateValue& value) {
+  this->copy((TemplateValue&) value);
+}
+
+void TemplateValue::copy(TemplateValue& value) {
+  _type = value._type;
+  if(_type == tString) {
+    vString = new string(value.getString());
+  } else if (_type == tBoolean) {
+    vBool = value.getBool();
+  } else if (_type == tInteger) {
+    vInt= value.getInt();
+  } else if (_type == tInteger64) {
+    vInt64 = value.getInt64();
+  } else if (_type == tDate64) {
+    vDate64 = new Date64(value.getDate());
+  } else if (_type == tVar) {
+    vVar = new TemplateVar(*value.vVar);
+  }
+}
+
+const TemplateValue& TemplateValue::operator = (const TemplateValue& copy) {
+  if (this == &copy) return *this;
+  this->copy((TemplateValue&) copy);
+  return *this;
+}
+
+
+
+TemplateValue::TemplateValue() {
+  vBool = false;
+  _type = tBoolean;
+}
+
+TemplateValue::TemplateValue(bool value) {
+  vBool = value;
+  _type = tBoolean;
+}
+
+TemplateValue::TemplateValue(int value) {
+  vInt = value;
+  _type = tInteger;
+}
+
+TemplateValue::TemplateValue(const string& value) {
+  vString = new string(value);
+  _type = tString;
+}
+
+TemplateValue::TemplateValue(__int64 value) {
+  vInt64 = value;
+  _type = tInteger64;
+}
+
+TemplateValue::TemplateValue(const Date64& value) {
+  vDate64 = new Date64(value);
+  _type = tDate64;
+}
+
+TemplateValue::TemplateValue(const TemplateVar& value) {
+  vVar = new TemplateVar(value);
+  _type = tVar;
+}
+
+void TemplateValue::clear() {
+  if (_type == tDate64) {
+    if (vDate64)
+      delete vDate64;
+  } else if (_type == tString) {
+    if (vString)
+      delete vString;
+  } else if (_type == tVar) {
+    if (vVar)
+      delete vVar;
+  }
+}
+
+string TemplateValue::getString() {
+  if(_type == tString) {
+    return *vString;
+  } else if (_type == tBoolean) {
+    return vBool ? "true" : "false";
+  } else if (_type == tInteger) {
+    return inttostr(vInt);
+  } else if (_type == tInteger64) {
+    return stringf("%ll", vInt64);
+  } else if (_type == tDate64) {
+    return stringf("%ll", vDate64->getInt64());
+  } else if (_type == tVar) {
+    return vVar->get().getString();
+  }
+  return "";
+}
+
+int TemplateValue::getInt() {
+  if(_type == tString) {
+    return atoi(vString->c_str());
+  } else if (_type == tBoolean) {
+    return vBool;
+  } else if (_type == tInteger) {
+    return vInt;
+  } else if (_type == tInteger64) {
+    return vInt64;
+  } else if (_type == tDate64) {
+    return vDate64->getTime64();
+  } else if (_type == tVar) {
+    return vVar->get().getInt();
+  }
+  return 0;
+}
+
+__int64 TemplateValue::getInt64() {
+  if (_type == tInteger64) {
+    return vInt64;
+  } else if (_type == tDate64) {
+    return vDate64->getInt64();
+  }
+  return getInt();
+}
+
+Date64 TemplateValue::getDate() {
+  if (_type == tDate64) {
+    return *vDate64;
+  }
+  return getInt64();
+}
+
+bool TemplateValue::getBool() {
+  return getInt();
+}
+
+TemplateValue::enTypes TemplateValue::getType() {
+  return _type;
+}
+
+TemplateValue TemplateValue::plus(TemplateValue& value) {
+  if (_type == tString) {
+    return TemplateValue(getString() + value.getString());
+  } else if (_type == tBoolean) {
+    return TemplateValue(getBool() + value.getBool()); //err
+  } else if (_type == tInteger) {
+    return TemplateValue(getInt() + value.getInt());
+  } else if (_type == tInteger64) {
+    return TemplateValue(getInt64() + value.getInt64());
+  } else if (_type == tDate64) {
+    return TemplateValue(Date64(Time64(getInt64() + value.getInt64())));
+  } else if (_type == tVar) {
+    return TemplateValue(this->vVar->get() + value);
+  }
+  return TemplateValue();
+}
+
+TemplateValue TemplateValue::minus(TemplateValue& value) {
+  if (_type == tString) {
+    //err
+  } else if (_type == tBoolean) {
+    //err
+  } else if (_type == tInteger) {
+    return TemplateValue(getInt() - value.getInt());
+  } else if (_type == tInteger64) {
+    return TemplateValue(getInt64() - value.getInt64());
+  } else if (_type == tDate64) {
+    return TemplateValue(Date64(Time64(getInt64() - value.getInt64())));
+  } else if (_type == tVar) {
+    return TemplateValue(this->vVar->get() - value);
+  }
+  return TemplateValue();
+}
+
+TemplateValue TemplateValue::comp(TemplateValue &value) {
+  if (_type == tString) {
+    return TemplateValue(getString() == value.getString());
+  } else if (_type == tBoolean) {
+    return TemplateValue(getBool() == value.getBool());
+  } else if (_type == tInteger) {
+    return TemplateValue(getInt() == value.getInt());
+  } else if (_type == tInteger64) {
+    return TemplateValue(getInt64() == value.getInt64());
+  } else if (_type == tDate64) {
+    return TemplateValue(getInt64() == value.getInt64());
+  } else if (_type == tVar) {
+    return TemplateValue(this->vVar->get() == value);
+  }
+  return TemplateValue();
+}
+
+TemplateValue TemplateValue::diff(TemplateValue &value) {
+  if (_type == tString) {
+    return TemplateValue(getString() != value.getString());
+  } else if (_type == tBoolean) {
+    return TemplateValue(getBool() != value.getBool());
+  } else if (_type == tInteger) {
+    return TemplateValue(getInt() != value.getInt());
+  } else if (_type == tInteger64) {
+    return TemplateValue(getInt64() != value.getInt64());
+  } else if (_type == tDate64) {
+    return TemplateValue(getInt64() != value.getInt64());
+  } else if (_type == tVar) {
+    return TemplateValue(this->vVar->get() != value);
+  }
+  return TemplateValue(true);
+}
+
+TemplateValue TemplateValue::and(TemplateValue &value) {
+  return TemplateValue(getBool() && value.getBool());
+}
+
+TemplateValue TemplateValue::or(TemplateValue &value) {
+  return TemplateValue(getBool() || value.getBool());
+}
+
+TemplateValue TemplateValue::not() {
+  return TemplateValue(!getBool());
+}
--- Source/Templates/TemplateValue.h
+++ Source/Templates/TemplateValue.h
@@ -3,47 +3,147 @@
 #ifndef __TEMPLATE_VALUE_H__
 #define __TEMPLATE_VALUE_H__
 
-class iTemplateValue {
-  public:
-  iTemplateValue& operator + (iTemplateValue* value) {
-    plus(value);
-    return *this;
-  }
-  iTemplateValue& operator - (iTemplateValue* value) {
-    minus(value);
-    return *this;
-  }
- // iTemplateValue& operator not (iTemplateValue* value) {
-  //  not(value);
- //   return *this;
-//  }
-  iTemplateValue& operator != (iTemplateValue* value) {
-    diff(value);
-    return *this;
-  }
-  iTemplateValue& operator == (iTemplateValue* value) {
-    comp(value);
-    return *this;
-  }
-  iTemplateValue& operator && (iTemplateValue* value) {
-    and(value);
-    return *this;
-  }
-  iTemplateValue& operator || (iTemplateValue* value) {
-    or(value);
-    return *this;
-  }
+#include <string>
+#include <Stamina/Time64.h>
+#include <Stamina/helpers.h>
 
-  private:
-    virtual void plus(iTemplateValue* value);
-    virtual void minus(iTemplateValue* value);
-    virtual void not(iTemplateValue* value);
-    virtual void comp(iTemplateValue* value);
-    virtual void and(iTemplateValue* value);
-    virtual void or(iTemplateValue* value);
-    virtual void diff(iTemplateValue* value);
+using namespace std;
+using namespace Stamina;
+
+class TemplateValue;
+
+class TemplateParam {
+public:
+  struct sWord {
+    bool not;
+    TemplateValue* value;
+
+    sWord(TemplateValue* value, bool not);
+  };
+
+  typedef vector<sWord*> tWords;
+
+  enum enOperators {
+    opPlus = 1,
+    opMinus = 2,
+    opNot = 3,
+    opComp = 4,
+    opDiff = 5,
+    opAnd = 6,
+    opOr = 7,
+  };
+
+public:
+  void addOperator(enOperators operator_);
+  void addWord(sWord& word);
+
+
+protected:
+  tWords _words;
 };
 
+class TemplateVar {
+public:
+  typedef vector<TemplateParam*> tParams;
+
+public:
+  TemplateVar(const string& name, bool isFunction = false);
+  TemplateVar(const TemplateVar& copy);
+  const TemplateVar& operator = (const TemplateVar& copy);
+  TemplateVar& operator = (TemplateVar& copy);
+  ~TemplateVar();
+
+public:
+  bool isFunction();
+  void copy(TemplateVar& value);
+  void addArg(TemplateParam& value);
+  void removeArg(UINT id);
+  TemplateValue get();
+
+protected:
+  string _name;
+  tParams _params;
+  bool _isFunction;
+};
+
+class TemplateValue {
+public:
+  enum enTypes {
+    tString = 1,
+    tInteger = 2,
+    tInteger64 = 3,
+    tBoolean = 4,
+    tDate64 = 5,
+    tVar = 6,
+  };
+
+  union {
+    string* vString;
+    int vInt;
+    __int64 vInt64;
+    bool vBool;
+    Date64* vDate64;
+    TemplateVar* vVar;
+  };
+
+public:
+  TemplateValue& operator + (TemplateValue& value) {
+    return plus(value);
+  }
+  TemplateValue& operator - (TemplateValue& value) {
+    return minus(value);
+  }
+  TemplateValue& operator! () {
+    return not();
+  }
+  TemplateValue& operator != (TemplateValue& value) {
+    return diff(value);
+  }
+  TemplateValue& operator == (TemplateValue& value) {
+    return comp(value);
+  }
+  TemplateValue& operator && (TemplateValue& value) {
+    return and(value);
+  }
+  TemplateValue& operator || (TemplateValue& value) {
+    return or(value);
+  }
+
+  virtual enTypes getType();
+  virtual void clear();
+  virtual void copy(TemplateValue& value);
+
+public:
+  TemplateValue();
+  TemplateValue(const string& value);
+  TemplateValue(int value);
+  TemplateValue(__int64 value);
+  TemplateValue(const Date64& value);
+  TemplateValue(bool value);
+  TemplateValue(const TemplateVar& value);
+  TemplateValue(const TemplateValue& value);
+  const TemplateValue& operator = (const TemplateValue& copy);
+  TemplateValue& operator = (TemplateValue& copy);
+
+public:
+  virtual string getString();
+  virtual bool getBool();
+  virtual int getInt();
+  virtual __int64 getInt64();
+  virtual Date64 getDate();
+
+protected:
+  virtual TemplateValue plus(TemplateValue& value);
+  virtual TemplateValue minus(TemplateValue& value);
+  virtual TemplateValue not();
+  virtual TemplateValue comp(TemplateValue& value);
+  virtual TemplateValue and(TemplateValue& value);
+  virtual TemplateValue or(TemplateValue& value);
+  virtual TemplateValue diff(TemplateValue& value);
+
+protected:
+  enTypes _type;
+};
 
 
 #endif // __TEMPLATE_VALUE_H__
